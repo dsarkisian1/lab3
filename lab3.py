9e65d7c61ca8a27739ef145bc4e38d03a4b09c14
David Sarkisian <dsarkisian@abbott.unx.cpp.edu>
2022-02-07 18:15:32 -0800

    Completed part 1

---
+++ lab3.py
@@ -0,0 +1,48 @@
+#name: David Sarkisian
+#Lab 3
+#Notes: Assignment is divided into multiple sections, will be seperated by 
+###############################################
+#################### PROBLEM # ################
+###############################################
+
+############################################### 
+#################### PROBLEM #1################
+############################################### 
+num1 = int(input("Enter a number: "))
+num2 = int(input("Enter a number: "))
+num3 = int(input("Enter a number: "))
+
+if num1 > num2 :
+   if num1 > num3 :
+      print(num1)
+   else :
+      print(num3)
+else :
+   if num2 > num3 :
+      print(num2)
+   else :
+      print(num3)
+
+#####OUTPUT#####
+
+#Enter a number: 3
+#Enter a number: 4
+#Enter a number: 5
+#5
+
+#Enter a number: 10
+#Enter a number: 20
+#Enter a number: 100
+#100
+
+#####GENERAL DESCRIPTION#####
+#This code appears to find the largest number in a group of 3
+
+
+
+############################################### 
+#################### PROBLEM #2################
+############################################### 
+
+
+
